7249d4c46a153f98330fce30b2aee5885cb4667d
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-12 00:19:44 -0500

    Add files via upload

---
+++ Strings/String3.py
@@ -0,0 +1,7 @@
+name = "Nate"
+print("Initial String: " + name) #<-----this is printing the intial string on the screen
+
+print("First character of the String is: " + name[0]) #<----- this will print the first character using indexing
+
+print("Last character of the String isL " + name[3]) #<----- this will print the last character using the index 
+
