28ab9aa717d02c9a1fe0ba34db9dd72ebd55e8ea
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-12 23:40:14 -0500

    Add files via upload

---
+++ Data Types/Data1.py
@@ -0,0 +1,11 @@
+name = "Nate" #<------ Strings
+
+number = 1 #<--------- Integer 
+
+print(float(number)) #<------ This will turn the number into a float which is 1.0
+
+print(1 == 2) #<------- results in a boolean of False, because 1 does not equal 2
+
+lst = [1,2,3,4,5] #<-------- a list of numbebrs
+
+dct = {"Nate":"Smart"} #<------ setting nate with a key of smart
