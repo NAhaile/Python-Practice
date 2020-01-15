150852b58862cf8bc25ca4d49f6bb4f0e9787a03
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-14 23:41:28 -0500

    Add files via upload

---
+++ Strings/String4.py
@@ -0,0 +1,6 @@
+#This program will print out a persons name and their age with string formatting
+
+name = "Nate"
+age = 20
+
+print("%s is %d years old."(name,age)) #<----- The %s symbolizes strings and %d is like numbers
