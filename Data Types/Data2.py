0b5310778f06eeee629fdd01fa60636a0dfafd9b
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-15 23:47:37 -0500

    Add files via upload

---
+++ Data Types/Data2.py
@@ -0,0 +1,8 @@
+numbers = [2,4]
+product = 1
+for i in numbers:
+    product = product * number #<----- we iterate through the numbers 2 and 4, and multiply it each time
+    
+
+print("The product is: ", product) #<----- Should give us 8
+
