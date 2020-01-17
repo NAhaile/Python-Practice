ab75bd17b1b52930e2da01a7baa8fa27bdd1cd28
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-16 23:59:07 -0500

    Update Rec.py

--- Recursion/Rec.py
+++ Recursion/Rec.py
@@ -1,10 +1,10 @@
 def recursive_add(n):
-    if n == 1:
+    if n == 1: #<---- Base case, if n ever becomes 1 then return 1 because you cant go any lower
         return n
     else:
-        return n+recursive_add(n-1)
+        return n+recursive_add(n-1) #<----- recursive call, this is where the magic happens
         
         
         
-print(recursive_add(5))
+print(recursive_add(5)) #<----- This will print out 15 ie: 5+4+3+2+1 = 15
     
