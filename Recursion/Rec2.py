eca1844247623a16170c461704294c2568122982
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-22 23:47:22 -0500

    Add files via upload

---
+++ Recursion/Rec2.py
@@ -0,0 +1,10 @@
+
+def recursive_add(n):
+    if n == 1: #<---- Base case, if n ever becomes 1 then return 1 because you cant go any lower
+        return n
+    else:
+        return n*recursive_add(n-1) #<----- recursive call, this is where the magic happens
+        
+        
+        
+print(recursive_add(5)) #<----- Similarly to addition we can do multiplication this will print out 120 ie: 5*4*3*2*1 = 120
