b0f820e365b6e3f19f8dc1c0ab7f0d0e73868e13
Natnael <nhaile2@masonlive.gmu.edu>
2020-03-20 23:57:25 -0400

    Update Fun2.py

--- Functions/Fun2.py
+++ Functions/Fun2.py
@@ -1,8 +1,17 @@
 #Reversing using the reverse() mathod in python
 
 def Rev(x):
-	x.reverse()
-	return x
+    x.reverse()
+    return x
+
+#Reversing using the slicing technique
+def Rev2(x):
+    var = x[::-1]
+    return var
+
+
 
 lst = [1,2,3,4]
-print(Rev(thing))
+print(Rev(lst))
+lst2 = [1,2,3,4]
+print(Rev2(lst2))
