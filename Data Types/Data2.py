d2fd725d45bb0219af021e2061c0a6c0ea0f8316
Natnael <nhaile2@masonlive.gmu.edu>
2020-04-16 23:57:04 -0400

    Update Data2.py

--- Data Types/Data2.py
+++ Data Types/Data2.py
@@ -1,7 +1,7 @@
 numbers = [2,4,3,8,9]
 product = 1
 for i in numbers:
-    product = product * number #<----- we iterate through the numbers 2 and 4, and multiply it each time
+    product = product * number #<----- we iterate through the numbers, and multiply it each time
     
 
 print("The product is: ", product) #<----- Should give us 24
